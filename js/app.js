5b083310ab93274f94a7b0fcd0261c9d8ec413b0
a1nukarsh <anukarsh@Anukarshs-MacBook-Pro.local>
2021-06-10 13:17:44 +0530

    files initialised

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log('This is notes taking app')
+
+// If a user adds a note, then add it to local storage
+
+// adding an event listener
+let addBtn=document.getElementById('addBtn')
+addBtn.addEventListener('click', function(e){
+    let addTxt=document.getElementById(addTxt)
+})
